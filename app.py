662cc6281d9e3a1d0d463624d032e8d58a851640
rayanalam504 <124403268+rayanalam504@users.noreply.github.com>
2023-08-16 13:11:49 +0100

    im so excited

--- app.py
+++ app.py
@@ -2,8 +2,45 @@
 
 import dash
 import dash_bootstrap_components as dbc
+# Import necessary libraries 
+from dash import html, dcc
+from dash.dependencies import Input, Output
+
+# Connect to your app pages
+from pages import page1, page2, page3, home
+
+# Connect the navbar to the index
+from components import navbar
+
 
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
                 meta_tags=[{"name": "viewport", "content": "width=device-width"}],
                 suppress_callback_exceptions=True)
+
+# define the navbar
+nav = navbar.Navbar()
+
+# Define the index page layout
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    nav, 
+    html.Div(id='page-content', children=[]),
+])
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/nbody':
+        return page1.layout
+    if pathname == '/orbits':
+        return page2.layout
+    if pathname == '/ptolemy':
+        return page3.layout
+    else:
+        return home.layout
+
+# Run the app on localhost:8050
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
