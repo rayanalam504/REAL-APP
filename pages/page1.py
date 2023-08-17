88c927cfe0dc9936bcc8ceb6a70f8529695bfc2d
rayanalam504 <124403268+rayanalam504@users.noreply.github.com>
2023-08-17 12:39:22 +0100

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -312,7 +312,7 @@ def submit_data(f_clicks, x_values, y_values, vx_values, vy_values, mass_values)
             ],
         [1,1,1]
     ]:
-        with open(r'freefall.json') as file:
+        with open(r'C:\Users\User\REAL APP\data\freefall.json') as file:
             temp = json.load(file)
         fig = adjust(go.Figure(temp))
         return "Data submitted successfully!", None, None, dcc.Graph(figure=fig)
